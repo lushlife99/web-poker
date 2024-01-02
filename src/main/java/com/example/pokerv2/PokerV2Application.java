3afbe861abb6c132e8b0871c279601626ef15232
lushlife99 <101994803+lushlife99@users.noreply.github.com>
2024-01-02 14:35:02 +0900

    first commit

---
+++ src/main/java/com/example/pokerv2/PokerV2Application.java
@@ -0,0 +1,13 @@
+package com.example.pokerv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokerV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pokerv2/PokerV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pokerv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
