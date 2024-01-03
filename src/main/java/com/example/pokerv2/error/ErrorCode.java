586c604d995564b9adba6b12688dbed3b0b76430
lushlife99 <101994803+lushlife99@users.noreply.github.com>
2024-01-03 20:46:41 +0900

    회원가입, 로그인, 리액트 3000포트 cors설정

--- src/main/java/com/example/pokerv2/error/ErrorCode.java
+++ src/main/java/com/example/pokerv2/error/ErrorCode.java
@@ -12,16 +12,17 @@ public enum ErrorCode {
 
     /* 401 UNAUTHORIZED : 인증되지 않은 사용자 */
 
+    NOT_EXISTS_USER(HttpStatus.UNAUTHORIZED, "존재하지 않는 userId 입니다"),
+    MISMATCH_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다"),
 
     /* 403 FORBIDDEN : 권한이 없는 사용자 */
-
 
 
     /* 404 NOT_FOUND : Resource 를 찾을 수 없음 */
 
 
     /* 409 CONFLICT : Resource 의 현재 상태와 충돌. 보통 중복된 데이터 존재 */
-    DUPLICATE_USER(HttpStatus.CONFLICT, "존재하는 userId 입니다"),
+    DUPLICATE_USER(HttpStatus.CONFLICT, "이미 존재하는 userId 입니다"),
 
 
     /* 500 INTERNAL_SERVER_ERROR : 서버오류 */
--- src/main/java/com/example/pokerv2/service/UserServiceV1.java
+++ src/main/java/com/example/pokerv2/service/UserServiceV1.java
@@ -1,1 +1,47 @@
+package com.example.pokerv2.service;
 
+import com.example.pokerv2.dto.UserDto;
+import com.example.pokerv2.error.CustomException;
+import com.example.pokerv2.error.ErrorCode;
+import com.example.pokerv2.model.User;
+import com.example.pokerv2.repository.UserRepository;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceV1 {
+
+    private final UserRepository userRepository;
+    private final SessionManager sessionManager;
+
+    public void join(String userId, String userName, String password){
+        Optional<User> findUser = userRepository.findByUserId(userId);
+        if(findUser.isPresent())
+            throw new CustomException(ErrorCode.DUPLICATE_USER);
+
+        User joinUser = User.builder().userId(userId)
+                .userName(userName)
+                .password(password).money(10000000).build();
+
+        userRepository.save(joinUser);
+
+    }
+
+    public UserDto login(String userId, String password, HttpServletResponse response) {
+        Optional<User> findUser = userRepository.findByUserId(userId);
+        if(findUser.isEmpty())
+            throw new CustomException(ErrorCode.NOT_EXISTS_USER);
+
+        User user = findUser.get();
+        if(!user.getPassword().equals(password))
+            throw new CustomException(ErrorCode.MISMATCH_PASSWORD);
+
+        sessionManager.createSession(user, response);
+        return new UserDto(user);
+    }
+
+}
