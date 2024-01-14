b13eeae18c33029eef4214c104ae0600f11e747a
lushlife99 <101994803+lushlife99@users.noreply.github.com>
2024-01-14 21:42:37 +0900

    update boardservice -> get, actionPos의 값이 빈 플레이어가 되는 오류 수정, add project init

---
+++ src/main/java/com/example/pokerv2/config/InitProject.java
@@ -0,0 +1,29 @@
+package com.example.pokerv2.config;
+
+import com.example.pokerv2.model.User;
+import com.example.pokerv2.service.UserServiceV1;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class InitProject {
+
+    private final UserServiceV1 userServiceV1;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void initData() {
+        log.info("Initial Data Init");
+        try {
+            userServiceV1.join(User.builder().userName("a1").password("1234").userId("a1").build());
+            userServiceV1.join(User.builder().userName("a2").password("1234").userId("a2").build());
+        } catch (Exception e) {
+            log.error("Initial fail");
+        }
+    }
+}
