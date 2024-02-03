2ac0ee1039072073f3f24b89ba7a62c368d4747b
lushlife99 <101994803+lushlife99@users.noreply.github.com>
2024-02-04 02:41:17 +0900

    add -> errorTest3

--- src/main/java/com/example/pokerv2/error/GlobalWebsocketExHandler.java
+++ src/main/java/com/example/pokerv2/error/GlobalWebsocketExHandler.java
@@ -1,37 +1,29 @@
 package com.example.pokerv2.error;
 
-
-import ch.qos.logback.classic.net.SocketReceiver;
 import com.example.pokerv2.dto.MessageDto;
 import com.example.pokerv2.enums.MessageType;
-import com.example.pokerv2.model.User;
 import com.example.pokerv2.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.security.Principal;
 
 @Slf4j
 @RequiredArgsConstructor
-@Component
+@RestControllerAdvice
 public class GlobalWebsocketExHandler {
 
     private final SimpMessagingTemplate simpMessagingTemplate;
     private final UserRepository userRepository;
 
-    @MessageExceptionHandler(CustomException.class)
+    @MessageExceptionHandler
     public void handleCustomException(CustomException ex, Principal principal) {
-        if (principal != null) {
-            userRepository.findByUserId(principal.getName())
-                    .ifPresent(u -> simpMessagingTemplate.convertAndSend("/queue/error/" + u.getId(), new MessageDto(MessageType.ERROR.toString(), ex)));
-        } else {
-            log.warn("Principal is null. Unable to handle exception.");
-        }
-
+        userRepository.findByUserId(principal.getName()).ifPresent(
+                u -> simpMessagingTemplate.convertAndSend("/queue/error/" + u.getId(), new MessageDto(MessageType.ERROR.toString(), ex.getMessage()))
+        );
     }
 
 }
