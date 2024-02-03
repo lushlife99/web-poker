c04099456cab29e8d6ac2fb6858e419d717cef04
lushlife99 <101994803+lushlife99@users.noreply.github.com>
2024-02-04 02:57:06 +0900

    add -> errorTest4

--- src/main/java/com/example/pokerv2/error/GlobalWebsocketExHandler.java
+++ src/main/java/com/example/pokerv2/error/GlobalWebsocketExHandler.java
@@ -5,6 +5,8 @@ import com.example.pokerv2.enums.MessageType;
 import com.example.pokerv2.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -18,12 +20,16 @@ public class GlobalWebsocketExHandler {
 
     private final SimpMessagingTemplate simpMessagingTemplate;
     private final UserRepository userRepository;
+    private static final String BOARD_ID = "board-id";
 
     @MessageExceptionHandler
-    public void handleCustomException(CustomException ex, Principal principal) {
-        userRepository.findByUserId(principal.getName()).ifPresent(
-                u -> simpMessagingTemplate.convertAndSend("/queue/error/" + u.getId(), new MessageDto(MessageType.ERROR.toString(), ex.getMessage()))
-        );
+    public void handleCustomException(CustomException ex, Message<?> message, Principal principal) {
+
+        String boardId = message.getHeaders().get(BOARD_ID).toString();
+        userRepository.findByUserId(principal.getName())
+                        .ifPresent(u -> {
+                            simpMessagingTemplate.convertAndSendToUser(principal.getName(), "/queue/error/" + boardId + "/" + u.getId(), new MessageDto(MessageType.ERROR.toString(), ex.getMessage()));
+                        });
     }
 
 }
