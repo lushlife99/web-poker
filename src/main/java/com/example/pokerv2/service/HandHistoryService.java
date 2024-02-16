a46b37ca63c301fb730ebf186603804a68e92ea6
lushlife99 <101994803+lushlife99@users.noreply.github.com>
2024-02-17 02:19:45 +0900

    disconnect_option null 체크 추가

--- src/main/java/com/example/pokerv2/service/HandHistoryService.java
+++ src/main/java/com/example/pokerv2/service/HandHistoryService.java
@@ -107,6 +107,7 @@ public class HandHistoryService {
                 HandHistory handHistory = userHandHistory.getHandHistory();
                 if(handHistory.isFinish()) {
                     handHistoryList.add(new HandHistoryDto(handHistory));
+                    System.out.println(new HandHistoryDto(handHistory));
                 }
             }
         }
